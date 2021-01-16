2186fbc29635be1ff513fe69d6a37cbf8039f8bd
ardasumbul1 <38762229+ardasumbul1@users.noreply.github.com>
2021-01-16 20:44:42 +0300

    Footer.css
    Footer design is upgraded.

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="main-footer">
     <div className="container">
-    <p id="footerp"><strong>Address:</strong>Özyeğin Üniversitesi Kampüsü Çekmeköy/İSTANBUL/TÜRKİYE</p>
+    <p id="footerp"><strong>Address:</strong> Özyeğin Üniversitesi Kampüsü Çekmeköy/İSTANBUL/TÜRKİYE</p>
     </div>
     <div className="footer-bottom">
     <p id="copyright">Copyright &copy;2021 Meme Page</p>
