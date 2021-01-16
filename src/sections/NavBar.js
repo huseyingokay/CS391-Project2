dc9481c9bf3be29413bb57f12561eeb5b7001d24
ardasumbul1 <38762229+ardasumbul1@users.noreply.github.com>
2021-01-16 14:17:54 +0300

    Navbar
    Other navigation bar components are added.

--- src/sections/NavBar.js
+++ src/sections/NavBar.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar,Nav} from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 
 
 
 class NavBar extends React.Component {
 
-  render(){
+  render() {
     return (
 
-        <>
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="/memes">Meme Page</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-  <Nav>
-      <Nav.Link href="/">Return To Login Page</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-        </>
+      <>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/memes">Meme Page</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="https://www.youtube.com/watch?v=BNqgTvC6krU" target="_blank">Eyvah! Mahmut Hoca</Nav.Link>
+              <Nav.Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Rick Roll</Nav.Link>
+              <Nav.Link href="https://www.youtube.com/watch?v=Z9XfN5wLKMU" target="_blank">Defend KEBAB</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/">Return To Login Page</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </>
 
     )
   }
