988ebeb0fdbc04f02f17b4f33e223bce7d5b2048
egeSPACEoztas <73328950+egeSPACEoztas@users.noreply.github.com>
2021-01-17 17:44:06 +0300

    Added password complexity:

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,10 +1,10 @@
 const validation = (userName, email, emailValidation, password, passwordValidation) => {
     const items = { ...localStorage };
 
-    if(userName == "" || email == "" || emailValidation == "" || password == "" || passwordValidation == ""){
-        console.log("You must enter all the blanks!");
-        alert("You must enter all the blanks!");
-        return false;
+    if (userName == "" || email == "" || emailValidation == "" || password == "" || passwordValidation == "") {
+        console.log("You must fill all the forms!");
+        alert("You must fill all the forms!");
+        return false; 
     }
 
     for (var index in items) {
@@ -21,13 +21,23 @@ const validation = (userName, email, emailValidation, password, passwordValidati
         alert("Please validate with correct Email!");
         return false;
     }
-    
+
+    var ExamplePassword = /^[A-Za-z]\w{7,14}$/;
+    if (password.value.match(ExamplePassword)) {
+        alert('Correct, try another...')
+        return true;
+    }
+    else {
+        alert('Wrong...!')
+        return false;
+    }
+
     if (password != passwordValidation) {
         console.log("Please validate with correct Password!");
         alert("Please validate with correct Password!");
         return false;
     }
-    
+
     return true;
 
 }
