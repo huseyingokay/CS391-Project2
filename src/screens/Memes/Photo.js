b0fccd0d85057c57ee190b196ac31734669d2618
Ayberk Akbalık <ayberk.akbalik@ozu.edu.tr>
2021-01-17 18:49:13 +0300

    Update Photo.js

--- src/screens/Memes/Photo.js
+++ src/screens/Memes/Photo.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card } from "react-bootstrap";
-import "./style.css";
+import './style.css';
 
 class Photo extends React.Component {
   render() {
     return (
+     <div class="memes">
       <Card className="mb-3">
-        <Card.Img src={this.props.link} />
-        <Card.Body>
-          <Card.Text className="writing">{this.props.description}</Card.Text>
-        </Card.Body>
+        <div class="img">
+         <Card.Img src={this.props.link} />
+        </div>
+        <div class="writing">
+         <Card.Body>
+          <Card.Text>{this.props.description}</Card.Text>
+         </Card.Body>
+        </div>
       </Card>
+    </div>
     );
   }
 }
